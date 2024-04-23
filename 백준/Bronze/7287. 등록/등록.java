3cc9c5784a17dbc46baf392e62afd3bde83bfd74
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-04-23 09:08:48 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.ArrayDeque;
+
+public class Main {
+
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        
+        bw.write("129\n");
+        bw.write("sunskyhyun");
+        bw.flush();
+    }
+}
