18a64b560ccf0470eb9b2ba45bfc9b26f77d3625
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-05-13 17:27:23 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -12,27 +12,16 @@ public class Main {
         String s = br.readLine();
 
         minus = s.split("-");
-
-        if(minus.length == 1){  // 음수부호가 1개도 없는경우 그냥 더해야함
-            plus = s.split("[+]");
+        for (int i = 0; i < minus.length; i++) {
+            plus = minus[i].split("[+]");       // 정규 표현식 문자 구분
             int tmp = 0;
             for (int j = 0; j < plus.length; j++) {
-                if(j == 0) answer = Integer.parseInt(plus[j]);
-                else tmp += Integer.parseInt(plus[j]);
+                tmp += Integer.parseInt(plus[j]);
             }
-            answer += tmp;
-        }else {
-            for (int i = 0; i < minus.length; i++) {
-                plus = minus[i].split("[+]");       // 정규 표현식 문자 구분
-                int tmp = 0;
-                for (int j = 0; j < plus.length; j++) {
-                    tmp += Integer.parseInt(plus[j]);
-                }
-                if(i == 0) answer = tmp;
-                else answer -= tmp;
-            }
+            if (i == 0) answer = tmp;
+            else answer -= tmp;
         }
-
+        
         bw.write(String.valueOf(answer));
         bw.flush();
     }
