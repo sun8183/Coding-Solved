1a0d1a8035d83cd9d67c3fbb28406f6f31401e48
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-05-24 18:52:46 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 31100 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    static int[] bridge;
+    static int[] city;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+
+        bridge = new int[N-1];
+        city = new int[N];
+
+        for (int i = 0; i < N-1; i++) {
+            bridge[i] = Integer.parseInt(st.nextToken());
+        }
+
+        s = br.readLine();
+        st = new StringTokenizer(s);
+
+        for (int i = 0; i < N; i++) {
+            city[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = city[0];
+
+        for (int i = 0; i < N-1; i++) {
+            if(min > city[i]){
+                min = city[i];
+
+            }
+
+            answer += min * bridge[i];
+        }
+        bw.write(String.valueOf(answer));
+        bw.flush();
+    }
+}
