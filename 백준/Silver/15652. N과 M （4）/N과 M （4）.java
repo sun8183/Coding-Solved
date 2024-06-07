6d16ecff74677466bf44176405f089cb80d05107
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-06-07 14:40:54 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 18116 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static int[] sel;
+
+    public static void main(String[] args) throws IOException {
+        String s = br.readLine();
+        N = Integer.parseInt(s.split(" ")[0]);
+        M = Integer.parseInt(s.split(" ")[1]);
+
+        sel = new int[M];      // 선택된 값을 넣는 배열 (N개 중 M개를 선택)
+
+        backTracking(0, 0);
+        bw.flush();
+    }
+
+    public static void backTracking(int nums, int start) throws IOException {
+        if(nums == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(sel[i]+" ");
+            }
+            bw.write("\n");
+        }
+        else{
+            for (int i = start; i < N; i++) {
+                sel[nums] = i+1;
+                backTracking(nums+1, i);
+            }
+        }
+    }
+
+}
