a5c2fbe70114613364a9d69a1da7a6ff2bc5a569
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-06-09 15:20:48 +0900

    [Silver III] Title: N과 M (6), Time: 152 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] sel;
+    static int[] check;
+    static int[] nums;
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        String s = br.readLine();
+        N = Integer.parseInt(s.split(" ")[0]);
+        M = Integer.parseInt(s.split(" ")[1]);
+
+        nums = new int[N];
+        check = new int[N];
+        sel = new int[M];
+
+        s = br.readLine();
+        StringTokenizer st = new StringTokenizer(s);
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        backTracking(0, 0);
+        bw.flush();
+    }
+
+    public static void backTracking(int target, int start) throws IOException {
+        if(target == M){
+            for (int i = 0; i < M; i++) {
+                bw.write(sel[i]+" ");
+            }
+            bw.write("\n");
+        }else{
+            for (int i = start; i < N; i++) {
+                if(check[i] == 0){
+                    check[i] = 1;
+                    sel[target] = nums[i];
+                    backTracking(target+1, i);
+                    check[i] = 0;
+                }
+            }
+        }
+    }
+}
