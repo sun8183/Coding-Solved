137136f5545f30a1ccde95b7726fb89f6427e21c
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-07-28 20:57:06 +0900

    [Bronze V] Title: 콜센터, Time: 120 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        bw.write("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+        bw.flush();
+    }
+}
