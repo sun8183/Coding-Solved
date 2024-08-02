2512a51c8d493e68fb9236f8702303b1a05b9020
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-08-02 22:35:00 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 120 ms, Memory: 15736 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static long N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        bw.write(N*(N-1)*(N-2)/6+"\n");
+        bw.write("3");
+        bw.flush();
+    }
+}
