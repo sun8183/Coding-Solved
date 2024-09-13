9411d142adc1e544f6a38fcbeddba886089c41d5
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-09-13 21:22:52 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        bw.write("고려대학교");
+        bw.flush();
+    }
+}
