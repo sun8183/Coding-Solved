da18075ea783048a8073eefc728adbd00144470c
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-09-16 19:34:51 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        bw.write(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+        bw.flush();
+    }
+}
