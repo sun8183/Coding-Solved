e67c94005acc0c19b68cce65aff63a0b73ec5941
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-10-13 21:50:27 +0900

    [Silver V] Title: K번째 수, Time: 1932 ms, Memory: 376684 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N, K;
+    static StringTokenizer st;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        bw.write(String.valueOf(arr[K-1]));
+        bw.flush();
+    }
+
+}
