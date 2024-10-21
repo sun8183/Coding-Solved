14f6939fdc256287d1ececae647bab9600fa8f7f
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-10-21 23:18:56 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 96 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N, K;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            br.readLine();
+        }
+        bw.write("비와이");
+        bw.flush();
+    }
+
+}
