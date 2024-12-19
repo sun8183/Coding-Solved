bd2af08547cb5dc638863d79d041682d571cab12
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-12-19 22:28:59 +0900

    [Bronze IV] Title: 입실 관리, Time: 104 ms, Memory: 14120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            bw.write(br.readLine().toLowerCase()+"\n");
+        }
+        bw.flush();
+    }
+
+}
+
+
+
+
