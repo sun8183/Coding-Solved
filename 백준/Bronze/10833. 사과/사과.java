50fc83b77a5ee468fca5f5387c63fa5df056e3ce
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2024-12-21 00:47:40 +0900

    [Bronze III] Title: 사과, Time: 108 ms, Memory: 14312 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    static StringTokenizer st;
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            answer += b%a;
+        }
+        bw.write(String.valueOf(answer));
+        bw.flush();
+    }
+
+}
+
+
+
+
