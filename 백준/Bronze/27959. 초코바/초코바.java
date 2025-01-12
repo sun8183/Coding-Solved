593297110d4047241ac1709221d3a0370cb2cbbf
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2025-01-12 21:35:14 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N, M;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        if(N * 100 >= M) bw.write("Yes");
+        else bw.write("No");
+
+        bw.flush();
+    }
+
+}
+
+
+
+
+
