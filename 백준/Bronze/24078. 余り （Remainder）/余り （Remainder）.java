ae9a211035003f5e24559620fb8f1b509311e4d4
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2025-02-08 01:46:59 +0900

    [Bronze V] Title: 余り (Remainder), Time: 100 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(N%21));
+        bw.flush();
+    }
+
+}
+
+
+
+
+
+
+
