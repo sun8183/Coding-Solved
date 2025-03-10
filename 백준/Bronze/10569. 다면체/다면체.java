0ccc5cbd40fdcd3829f7075656924b7f4775cdf7
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2025-03-10 22:40:54 +0900

    [Bronze III] Title: 다면체, Time: 108 ms, Memory: 15800 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int T;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            bw.write(2 - V + E+"\n");
+        }
+
+        bw.flush();
+    }
+
+}
+
+
+
+
+
+
+
+
+
