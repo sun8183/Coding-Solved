6832b99c3e3a1221cb63f68d87e0f2677bd1a6e9
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2025-05-23 23:54:43 +0900

    [Bronze V] Title: Serca, Time: 224 ms, Memory: 15600 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+
+            bw.write(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @    ");
+
+            bw.write("\n");
+
+        }
+        bw.flush();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
