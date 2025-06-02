f3e2bbb489f4aa1e57265e448495a36c049c3e26
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2025-06-02 18:56:02 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 14112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int N;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            bw.write("SciComLove\n");
+        }
+        bw.flush();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
