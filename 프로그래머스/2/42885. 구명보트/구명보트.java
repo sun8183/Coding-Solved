41998dd5c76cde5c823616e2813b30ccee5c0a6e
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2026-07-07 21:45:34 +0900

    [level 2] Title: 구명보트, Time: 9.36 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length-1;
+        
+        Arrays.sort(people);
+        
+        while(left <= right){
+            if(people[left] + people[right] <= limit) {
+                answer++;
+                left++;
+                right--;
+            }else if(people[right] <= limit){
+                answer++;
+                right--;
+            }
+        }
+        
+        return answer;
+    }
+}
