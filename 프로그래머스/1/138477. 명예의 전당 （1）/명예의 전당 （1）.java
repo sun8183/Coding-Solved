1317f4a8861a37a165e184b7cf7a53eb81d085d6
LimTaeYang <89739518+sun8183@users.noreply.github.com>
2026-07-20 15:38:44 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.71 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        Queue<Integer> queue = new PriorityQueue<>();
+        
+        for(int i=0; i<score.length; i++){
+            queue.add(score[i]);
+            if(queue.size() > k) queue.poll();
+            answer[i] = queue.peek();
+        }
+        
+        return answer;
+    }
+}
